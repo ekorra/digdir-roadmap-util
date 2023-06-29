3da800c0f8c69c25b5aad96b6b89ac9438267669
ekorra <ekorra@gmail.com>
2023-06-29 13:56:15 +0200

    trig report from Github

---
+++ GithubRelease.py
@@ -0,0 +1,48 @@
+import logging
+import logging.handlers
+import os
+import shutil
+import datetime
+from Helpers import *
+from DigdirRoadmap import *
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+try:
+    roadmap_token = os.environ["DIGIDIR_ROADMAP_TOKEN"]
+except KeyError:
+    roadmap_token = "Token not available!"
+    logger.error("Token not available!")
+    # raise
+
+
+date = datetime.datetime.now()
+formated_date = date.strftime("%d%m%Y_%H%S")
+week = date.strftime("%W")
+
+
+def main() -> any:
+    with open("included_projects.txt", "r") as reader:
+        filter = reader.read().splitlines()
+
+    roadmap_issues = getDigdirRoadmap(roadmap_token, filter)
+    csvfile = generate_csv(roadmap_issues)
+    with open(f"output/roadmap_report.csv", "w") as stream:
+        csvfile.seek(0)
+        shutil.copyfileobj(csvfile, stream)
+
+
+if __name__ == "__main__":
+    logger.info(f"Token value: {roadmap_token}")
+    main()
