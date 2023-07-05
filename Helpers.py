bbe6e029c7830b8a92f7e459f6d4aa744bd48717
ekorra <ekorra@gmail.com>
2023-07-05 07:49:54 +0200

    Added mailnotification with github release link

--- Helpers.py
+++ Helpers.py
@@ -8,37 +8,53 @@ import io
 import csv
 
 
-def send_email(subject: str, body, sender: str, recipients: list, password: str, report_file: io.StringIO, report_file_name: str):
-
-    part = MIMEBase("application", "octet-stream")
-    part.set_payload(report_file.read())
-    encoders.encode_base64(part)
-    part.add_header(
-        "Content-Disposition",
-        f"attachment; filename= {report_file_name}.csv")
+def send_email(subject: str, body, sender: str, recipients: list, password: str, report_file: io.StringIO = None, report_file_name: str = None):
 
     msg = MIMEMultipart()
     msg['Subject'] = subject
     msg['From'] = sender
     msg['To'] = ', '.join(recipients)
 
-    html_part = MIMEText(body)
+    html_part = MIMEText(body, 'html')
     msg.attach(html_part)
-    msg.attach(part)
+
+    if report_file is not None:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(report_file.read())
+        encoders.encode_base64(part)
+        part.add_header(
+            "Content-Disposition",
+            f"attachment; filename= {report_file_name}.csv")
+        msg.attach(part)
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
         smtp_server.login(sender, password)
         smtp_server.sendmail(sender, recipients, msg.as_string())
-    print("Message sent!")
 
 
 def generate_csv(roadmapItems: DigdirRoadmapItem):
     csv_output = io.StringIO()
     csv_writer = csv.writer(csv_output)
-    csv_writer.writerow(["Tittel", "Produkt", "Status", "Start",
-                        "End", "Progresjon (%)", "Storypoints", "Issues", "Closed", "URL"])
+    csv_writer.writerow(["Tittel",
+                        "Produkt",
+                         "Status",
+                         "Start",
+                         "End",
+                         "Progresjon (%)",
+                         "Storypoints",
+                         "Issues",
+                         "Closed",
+                         "URL"])
     for roadmap_item in roadmapItems:
-        csv_writer.writerow([roadmap_item.title, roadmap_item.product, roadmap_item.status, roadmap_item.start,
-                             roadmap_item.end, roadmap_item.progresjon, roadmap_item.storypoints, roadmap_item.numberOfTrackedIssues, roadmap_item.numberOfSovedIssues, roadmap_item.url])
+        csv_writer.writerow([roadmap_item.title,
+                            roadmap_item.product,
+                            roadmap_item.status,
+                            roadmap_item.start,
+                            roadmap_item.end,
+                            roadmap_item.progresjon,
+                            roadmap_item.storypoints,
+                            roadmap_item.numberOfTrackedIssues,
+                            roadmap_item.numberOfSovedIssues,
+                            roadmap_item.url])
     csv_output.seek(0)
     return csv_output
---
+++ mail_notifier.py
@@ -0,0 +1,56 @@
+import logging
+import logging.handlers
+import yaml
+import datetime
+import requests
+import os
+from Helpers import send_email
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+
+def main() -> None:
+    with open("config.yml", "r") as stream:
+        config = yaml.safe_load(stream)
+
+    if config is not None:
+        reciepients = config["mail_receipients"]
+        shold_send_mail = config["send_notification_mail"]
+        release_url = config["latest_release_url"]
+
+    if (shold_send_mail is None or reciepients is None or release_url is None):
+        return
+
+    response = requests.get(release_url)
+    if response.status_code != 200:
+        return
+
+    response_json = response.json()
+    release_name = response_json["name"]
+    report_link = response_json["assets"][0]["browser_download_url"]
+    release_link = response_json["html_url"]
+
+    date = datetime.datetime.now()
+    week = date.strftime("%W")
+    subject = f"Digidir roadmap rapport uke {week}"
+    body = f"{release_name}<br>Rapport for uke {week} kan lastes ned ved å trykke <a href={report_link}>her</a><br>Eventult kan du gå <a href={release_link}>hit</a> og laste den ned"
+
+    password = os.getenv("SMTP_PASSWORD")
+    sender = os.getenv("SMTP_ACCOUNT")
+
+    send_email(subject, body, sender, reciepients, password)
+
+
+if __name__ == "__main__":
+    main()
